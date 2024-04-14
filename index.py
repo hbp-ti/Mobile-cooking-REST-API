677026c3fe7fa2078773cff01d5aab892055f7c1
Hugo Pereira <hbp@ua.pt>
2024-04-14 15:37:24 +0100

    fix

--- index.py
+++ index.py
@@ -1,6 +1,14 @@
-from flask import Flask
+import os
+from datetime import datetime, timedelta
+from functools import wraps
+import jwt
+import psycopg2
+from flask import Flask, jsonify, request
+
+import db
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
@@ -9,3 +17,6 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == "__main__":
+    app.run()
