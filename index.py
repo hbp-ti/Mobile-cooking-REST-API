3a88fc31a8b52b5406a511799ab926381d2adb46
Hugo Pereira <hbp@ua.pt>
2024-04-21 15:40:15 +0100

    added functionalities to login, create and change user

--- index.py
+++ index.py
@@ -92,6 +92,9 @@ def update_user(id_user):
     if "name" not in data or "email" not in data or "username" not in data or "password" not in data:
         return jsonify({"error": "invalid parameters"}), BAD_REQUEST_CODE
 
+    if (db.user_exists(data)):
+    return jsonify({"error": "user already exists"}), BAD_REQUEST_CODE
+
     user = db.change_user(id_user, data)
 
     return jsonify(user), SUCCESS_CODE
