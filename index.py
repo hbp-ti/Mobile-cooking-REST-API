b0e8b1d48a27ed828bf4c0b9cb20a5bbd2f9676f
Hugo Pereira <hbp@ua.pt>
2024-04-28 23:59:03 +0100

    added validations, network access, and create account & login volley requests

--- index.py
+++ index.py
@@ -53,10 +53,10 @@ def register():
         return jsonify({"error": "invalid parameters"}), BAD_REQUEST_CODE
 
     if (db.user_exists(data)):
-        return jsonify({"error": "user already exists"}), BAD_REQUEST_CODE
+        return jsonify({"error": "User already exists"}), BAD_REQUEST_CODE
 
     if (db.email_exists(data)):
-        return jsonify({"error": "email already exists"}), BAD_REQUEST_CODE
+        return jsonify({"error": "Email already exists"}), BAD_REQUEST_CODE
 
     user = db.add_user(data)
 
